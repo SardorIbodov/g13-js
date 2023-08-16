7263d8ab20bae0612e6c4590a01c48ce3981b7ba
Sardor <sardoribodov07@gmail.com>
2023-08-16 16:46:45 +0500

    2. Github

---
+++ 2. Github/index.js
@@ -0,0 +1,20 @@
+//! git init => proyektga git ni initsializatsiya qiladi (proyekt yaratilganda 1 marta)
+
+//? Github ga qaysi username va email orqali kod qo'shilgani
+//! git config --global user.name "" (proyekt yaratilganda 1 marta)
+//! git config --global email.name "" (proyekt yaratilganda 1 marta)
+
+//? Githubga proyektni ulash
+//! git remote add origin URL (proyekt yaratilganda 1 marta)
+//! git remote => oraliqda papkalarni ko'rsatadi
+//! git remote remove papkaNomi => o'chiradi
+
+//? Origin papkaga ma'lumot qo'shish
+//! git add file
+//! git add . => proyektdagi hamma faylni qo'shadi
+
+//? Izoh berish
+//! git commit -m "Izoh teksti"
+
+//? push
+//! git push origin master
