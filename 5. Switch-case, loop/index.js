f64e9461fcbcff8f49358c35a7c5a7293636195a
Sardor <sardoribodov07@gmail.com>
2023-08-21 17:17:16 +0500

    Switch-case

---
+++ 5. Switch-case, loop/index.js
@@ -0,0 +1,39 @@
+//! Switch-case
+//? Hafta kunlari (Mon => Monday)
+// let day = "Friday";
+// switch(day) {
+// 	case "Monday": console.log("Mon"); break;
+// 	case "Tuesday": console.log("Tue"); break;
+// 	case "Wednesday": console.log("Wed"); break;
+// 	case "Thursday": console.log("Thu"); break;
+// 	case "Friday": console.log("Fri"); break;
+// 	case "Saturday": console.log("Sat"); break;
+// 	case "Sunday": console.log("Sun"); break;
+// 	default: console.log("Noto'g'ri ma'lumot kiritildi!");
+// }
+
+//? Simple calculator
+// let operator = "/";
+// let a = 2, b = 5;
+// switch(operator) {
+// 	case "+": console.log(a + b); break; 
+// 	case "-": console.log(a - b); break;
+// 	case "*": console.log(a * b); break;
+// 	case "/": console.log(a / b); break;
+// 	default: console.log("Noto'g'ri ma'lumot kiritildi!");
+// }
+// console.log(select);
+
+//? Greetings
+// let language = "en";
+// switch(language) {
+// 	case "en": console.log("Hello"); break;
+// 	case "uz": console.log("Salom"); break;
+// 	case "esp": console.log("Hola"); break;
+// 	case "ru": console.log("Privet"); break;
+// }
+// if(language === "en") console.log("Hello");
+// else if(language === "uz") console.log("Salom");
+// else if(language === "esp") console.log("Hola");
+// else if(language === "ru") console.log("Privet");
+// else console.log("404");
