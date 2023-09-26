e817cdcd1c5f1945c5d3c108962d8750a4b88864
Sardor <sardoribodov07@gmail.com>
2023-09-26 07:01:41 +0500

    setInterval | setTimeout

---
+++ 19. setInterval, setTimeout/index.js
@@ -0,0 +1,18 @@
+//! Sinxron va Asinxron
+//? sinxron kod
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+//! setTimeout(function, milliseconds), setInterval
+// console.log(1);
+// setTimeout(() => {
+//   console.log(2.1);
+// }, 0);
+// setTimeout(() => {
+//   console.log(2.2);
+// }, 1000);
+// console.log(3);
+// for (let i = 0; i <= 1000; i++) {}
+// console.log("tugadi");
+// 1 3 tugadi 2.1 2.2
