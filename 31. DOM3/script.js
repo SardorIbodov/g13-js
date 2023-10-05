d6cd863a9da8da592a7cbb81e0fd73d57d91d3db
Sardor <sardoribodov07@gmail.com>
2023-10-05 10:57:32 +0500

    DOM | 3

---
+++ 31. DOM3/script.js
@@ -0,0 +1,33 @@
+// const button = document.querySelector("button");
+// const box = document.querySelector(".box");
+// function clicked(some) {
+//   console.log(some);
+// }
+// button.onclick = () => clicked("text");
+//? click
+// button.addEventListener("click", (e) => {
+//   console.log(e, 1);
+// });
+//? contextmenu
+// button.addEventListener("contextmenu", () => console.log(1));
+//? dblclick
+// button.addEventListener("dblclick", () => alert("Siz 2 marta bosdingiz!"));
+//? mouseover
+// let isInside = false;
+// box.addEventListener("mouseover", () => {
+//   box.style.backgroundColor = "green";
+// });
+//? mouseout
+// box.addEventListener("mouseout", () => {
+//   box.style.backgroundColor = "red";
+// });
+//? mousemove
+// box.addEventListener("mousemove", (e) => {
+//   console.log(e.clientX, "x");
+//   console.log(e.clientY, "y");
+//   box.style.backgroundColor = `rgb(${e.clientX}, ${e.clientY}, ${e.clientX})`;
+// });
+//? mouseup
+// button.addEventListener("mouseup", () => console.log(1))
+//? mousedown
+// button.addEventListener("mousedown", () => console.log(1));
